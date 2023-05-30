4e1ea8e15675284b9c983ef7e09ac56fa8c07de3
OmarHesham <omarhesham11a@gmail.com>
2023-05-30 23:07:59 +0300

    commmmmmit

--- app/amr test/tset2.php
+++ app/amr test/tset2.php
@@ -1,6 +1,13 @@
 <?php
-class amr {
-    public function hello2(){
+class amr
+{
+    public function hello2()
+    {
+        echo "amr2";
+    }
+
+    public function hello3()
+    {
         echo "amr2";
     }
 }
